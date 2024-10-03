c608a241561254da6ecfe3839221f2adc0aace52
jasontcrabtree <jasontcrabtree@gmail.com>
2024-10-03 22:56:47 +1300

    Initial Commit

---
+++ hooper-v2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  hooper-v2
+//
+//  Created by Jason Crabtree on 03/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hooper-v2/hooper_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  hooper_v2App.swift
+//  hooper-v2
+//
+//  Created by Jason Crabtree on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct hooper_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
